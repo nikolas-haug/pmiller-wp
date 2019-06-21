c945179995d4e063af0f4c62878f6119dce653bf
Nikolas Haug <nikolas.haug@futurenet.com>
2019-06-21 14:30:04 -0500

    add last archive page

---
+++ single-commercial.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+<h1>Single-Commercial.php</h1>
+
+<?php while (have_posts()) : the_post(); ?>
+
+    <h2><?php the_title(); ?></h2>
+
+    <div>
+        <?php the_content(); ?>
+    </div>
+    
+    <p><?php the_excerpt() ?></p>
+
+<? endwhile; wp_reset_query(); ?>
+
+
+<?php get_footer(); ?>
